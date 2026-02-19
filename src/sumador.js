e130506c0796f1281e9259f32d81e8cb73a97928
Rubén Cordero <garu1812@gmail.com>
2026-02-19 10:25:59 -0400

    jest implementado

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,11 @@
 function sumar(a, b) {
   return a + b;
 }
+function restar(a, b) {
+  return a - b;
+}
+function multiplicar(a, b) {
+  return a * b;
+}
 
-export default sumar;
+export { sumar, restar, multiplicar };
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,9 +1,15 @@
-import sumar from "./sumador.js";
+import { sumar, restar, multiplicar } from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
+
+  it("deberia restar dos numeros", () => {
+    expect(restar(5, 1)).toEqual(4);
+  });
+
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(3, 2)).toEqual(6);
+  });
 });
-
-
